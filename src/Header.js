8d14d7adfd215941e17b25c181fa69eb6a5b1b02
JetechNY <sunjetliu@gmail.com>
2021-03-24 18:01:49 -0400

    changes

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 function Header() {
+
+  function handleClick(event){
+    console.log(event)
+  }
+
   return (
     <div>
       <h1> SunJet's Todo App </h1>
+      <button onClick={handleClick}>Click me</button>
       <p> Please enter a task </p>
     </div>
   );
