0297e01ad988e227b17fd08a5ac8a6668de23552
JetechNY <sunjetliu@gmail.com>
2021-03-25 23:32:31 -0400

    changes

--- src/SignUp.js
+++ src/SignUp.js
@@ -3,16 +3,18 @@ import React, { useState } from "react";
 function SignUp() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [accountType, setAccountType] = useState("user")
 
   function handleSubmit(event) {
     event.preventDefault();
       const formData = {
         username,
         password,
+        accountType,
       }
       console.log(formData);
     }
-  
+
 
   return (
     <form onSubmit={handleSubmit}>
@@ -32,6 +34,16 @@ function SignUp() {
         onChange={(e)=> setPassword(e.target.value)}
       />
       <input type="submit" value="Sign Up" />
+      <label> Account Type </label>
+      <select
+        id="type"
+        value={accountType}
+        onChange={(e)=>setAccountType(e.target.value)}
+      >
+        <option value="user">User</option>
+        <option value="admin">Admin</option>
+        <option value="manager">Manager</option>
+      </select>
     </form>
   );
 }
